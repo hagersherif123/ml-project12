5027ccf3629db54536507c71769605fef8bd0931
hagersherif123 <hagar403839@fci.bu.edu.eg>
2024-12-16 16:06:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import streamlit as st
 # تحميل الملف من Google Drive
 def download_file_from_drive(file_url):
     file_id = file_url.split('/d/')[1].split('/')[0]
-    download_url = f"https://drive.google.com/uc?id={file_id}"
+    download_url = f"https://drive.google.com/uc?id={file_id}&export=download"
     
     response = requests.get(download_url)
     if response.status_code == 200:
@@ -45,7 +45,7 @@ def main():
     st.title("🩺 Diabetes Prediction App")
     st.write("Enter the patient's details below to predict the likelihood of diabetes.")
 
-    # Define the Google Drive links
+    # Define the Google Drive links (with direct download)
     model_url = "https://drive.google.com/file/d/1Cx91Q_2AlsfidDzktxrCxCM3C3suZQGC/view?usp=sharing"
     log_transform_url = "https://drive.google.com/file/d/1URz0ERV8mycKj9M2JABAUHNfq7qVyRxo/view?usp=sharing"
 
